534fa04b29b9ec749b16b7721169efb6cfe17df5
Maxim Kovalev <maxim.kovalev@west.cmu.edu>
2013-10-30 04:42:33 -0700

    Added bundle processing.

--- locationpythonserver/locationpythonserver.py
+++ locationpythonserver/locationpythonserver.py
@@ -7,6 +7,43 @@ import cgi
 import json
 import MySQLdb
 
+def process_one_obj(obj):
+    timestamp = obj['timestamp']
+    print 'Timestamp:', timestamp
+    location = obj['location']
+    print location
+    db = MySQLdb.connect(host = 'localhost', user = 'root', db = 'wifilocation')
+    db.set_character_set('utf8')
+    c = db.cursor()
+    c.execute('SET NAMES utf8;')
+    c.execute('SET CHARACTER SET utf8;')
+    c.execute('SET character_set_connection=utf8;')
+    query = "SELECT location_id FROM locations WHERE location_name = \'%s\'" % location
+    while c.execute(query) != 1:
+        c.execute("INSERT INTO locations (location_name) VALUES (\'%s\')" % location)
+        db.commit()
+    location_id = int(c.fetchone()[0])
+            
+    for key in obj.keys():
+        if key[:9] == "wifiBSSID":
+            bssid = key[9:]
+            level = obj[key]
+            print bssid, ":", level
+            query = "INSERT INTO wifi_readings (timestamp, location_id, BSSID, level) VALUES (%d, %d, \'%s\', %d)" % (timestamp, location_id, bssid, level)
+            print query
+            c.execute(query)
+
+    if obj.has_key("GPSLat"):
+        if obj.has_key("Cellular_signal"):
+            query = "INSERT INTO gps_and_signal_readings (timestamp, location_id, Longitude, Latitude, Cellular_signal) VALUES (%d, %d, %f, %f, %d)" % (timestamp, location_id, obj['GPSLon'], obj['GPSLat'], obj['Cellular_signal'])
+        else:
+            query = "INSERT INTO gps_and_signal_readings (timestamp, location_id, Longitude, Latitude) VALUES (%d, %d, %f, %f)" % (timestamp, location_id, obj['GPSLon'], obj['GPSLat'])
+        print query
+        c.execute(query)
+    c.close()
+    db.commit()
+    db.close()
+
 class HTTPRequestHandler(BaseHTTPRequestHandler):
  
     def do_POST(self):
@@ -19,37 +56,15 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         print "Data:", data
         self.send_response(200, "Roger that")
         obj = json.loads(data.keys()[0])
-        timestamp = obj['timestamp']
-        print 'Timestamp:', timestamp
-        location = obj['location']
-        print location
-        db = MySQLdb.connect(host = 'localhost', user = 'root', db = 'wifilocation')
-        c = db.cursor()
-        query = "SELECT location_id FROM locations WHERE location_name = \'%s\'" % location
-        while c.execute(query) != 1:
-            c.execute("INSERT INTO locations (location_name) VALUES (\'%s\')" % location)
-            db.commit()
-        location_id = int(c.fetchone()[0])
-        
-        for key in obj.keys():
-            if key[:9] == "wifiBSSID":
-                bssid = key[9:]
-                level = obj[key]
-                print bssid, ":", level
-                query = "INSERT INTO wifi_readings (timestamp, location_id, BSSID, level) VALUES (%d, %d, \'%s\', %d)" % (timestamp, location_id, bssid, level)
-                print query
-                r= c.execute(query)
-                print "response:", r
-                for l in c.fetchall():
-                        print l
-                print '\n'
-        if obj.has_key("GPSLat"):
-            pass
-                
-        c.close()
-        db.commit()
-        db.close()
+        if isinstance(obj, list):
+            print "Processing a list"
+            for o in obj:
+                process_one_obj(o)
+        elif isinstance(obj, dict) and obj.has_key('timestamp') and obj.has_key('location'):
+            print "Processing single dict"
+            process_one_obj(obj)
         print '\n'
+        
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     allow_reuse_address = True

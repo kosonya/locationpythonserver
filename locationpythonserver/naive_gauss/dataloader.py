9082f95e2a675868357bb8b1714420550fe88dac
Maxim Kovalev <maxim.kovalev@west.cmu.edu>
2013-11-13 11:33:03 -0800

    GPL

--- locationpythonserver/naive_gauss/dataloader.py
+++ locationpythonserver/naive_gauss/dataloader.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
-
+#    Copyright (c) 2013 Maxim Kovalev, Carnegie Mellon University
+#    This file is part of Locationing Server.
+#
+#    Locationing Server is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Locationing Server is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Locationing Server.  If not, see <http://www.gnu.org/licenses/>.
 import MySQLdb
 
 db_name = 'wifilocation'
